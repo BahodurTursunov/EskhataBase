bcef6db3fb261bcfea641727c3eeaa438eeb4937
RomanovSV <slava@litiko.com>
2024-07-03 18:08:42 +0300

    Импорт пакета разработки [Rx]

---
+++ DirRX.ADIntegrationCore/DirRX.ADIntegrationCore.ClientBase/ModuleClientFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sungero.Core;
+using Sungero.CoreEntities;
+
+namespace DirRX.ADIntegrationCore.Client
+{
+  /// <summary>
+  /// Класс клиентских функций модуля.
+  /// </summary>
+  public class ModuleFunctions
+  {
+    
+  }
+}
